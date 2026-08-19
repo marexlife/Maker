7ea8af6aedc5da1f8488692c6e2414240f6fab73
marcel <marcel.hajek7@gmail.com>
2026-08-19 12:56:27 +0200

    chnaged help arg to not conflict with the dotnet run one

--- Config/CommandConfig.cs
+++ Config/CommandConfig.cs
@@ -2,7 +2,7 @@ namespace Maker.Config;
 
 internal static class CommandConfig
 {
-    internal const string HelpCommand = "--help";
+    internal const string HelpCommand = "help";
     internal const string ModuleCommand = "mod";
     internal const string ProjectCommand = "project";
 }
