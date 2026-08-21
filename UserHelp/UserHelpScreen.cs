6e6cc946df08ce0834812f1c8d37785a602910a2
marcel <marcel.hajek7@gmail.com>
2026-08-21 11:30:53 +0200

    added clear command to help screen

--- UserHelp/UserHelpScreen.cs
+++ UserHelp/UserHelpScreen.cs
@@ -10,6 +10,7 @@ internal static class UserHelpScreen
     Use '{CommandConfig.ModuleCommand} my_module' to create a new project module.
     Use '{CommandConfig.ProjectCommand} my_project' to create a new project.
     Use '{CommandConfig.RunCommand}' to run your project.
+    Use '{CommandConfig.ClearCommand}' to clear your project.
     """;
 
     internal static void DisplayHelp()
